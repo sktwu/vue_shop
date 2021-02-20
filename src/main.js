1ae40e6f12085ab680d7cf8dd707d4080031cd08
NagatoYuki <1539335259@qq.com>
2021-02-20 17:43:22 +0800

    登录功能实现

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
+
+// 导入全局样式
+import './assets/style/global.css'
+import './assets/style/font-awesome.min.css'
+
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
